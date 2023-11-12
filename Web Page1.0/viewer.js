6a49e5efd6f14c4ef80bec0ef9ab0daca2c9ab79
bondlegend0110 <jose_sandoval@brown.edu>
2023-11-12 03:26:51 -0500

    Curved Volume Integrate Singular Form pt 1 This is the code that generates a preview of the stl and combines onto a singular template. Has the 4 views, allows for indepedent rotation of each onto final template to account for folding. TODO: include form selection which is present but not accessed.     This requires greater abstraction. Also neccesary to work on the canvas and controls so that the page is more streamline and so the views can be controled by the singular set of controls, respectively

--- Web Page1.0/viewer.js
+++ Web Page1.0/viewer.js
@@ -1,22 +1,54 @@
 //no rotation on z for the six sides
-function stlLoad(file,stlName,number){
+function stlLoadCurve(file){
 
     var canvasList = document.getElementsByTagName("canvas");
 
     for (var i = 0, len = canvasList.length; i < len; i++) {
-        canvasList[number].remove();
+        canvasList[0].remove();
     }
 
-    var stl_viewer = new StlViewer ( document.getElementById(stlName) );
+    // the views are front or back. The divs that hold them are on the left or right side.
+    var stl_viewer_front = new StlViewer ( document.getElementById("stl_view1") );
+    stl_viewer_front.add_model ( {
+        id: 1,
+        local_file:file,
+        rotationx:-0.5 * 3.14,
+        rotationy: 0,
+        rotationz: 0,
+        auto_resize: true,
+    });
+    stl_viewer_front.set_auto_resize(true);
 
-    stl_viewer.add_model ( {
-        id: number,
+    var stl_viewer_back = new StlViewer ( document.getElementById("stl_view2") );
+    stl_viewer_back.add_model ( {
+        id: 2,
         local_file:file,
         rotationx: 0.5 * 3.14,
         rotationy: 0,
         rotationz: 0,
         auto_resize: true,
     });
+    
+    var stl_viewer_right = new StlViewer ( document.getElementById("stl_view3"));
+    stl_viewer_right.add_model ( {
+        id: 0,
+        local_file:file,
+        rotationx:-0.5 * 3.14,
+        rotationy: 0,
+        rotationz:-0.5 * 3.14,
+        auto_resize: true,
+    });
+
+    var stl_viewer_left = new StlViewer ( document.getElementById("stl_view4"));
+    stl_viewer_left.add_model ( {
+        id: 0,
+        local_file:file,
+        rotationx:-0.5 * 3.14,
+        rotationy: 0,
+        rotationz: 0.5 * 3.14,
+        auto_resize: true,
+    });
+
 }
 
 function loadBasic(){
