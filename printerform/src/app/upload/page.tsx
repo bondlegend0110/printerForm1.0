94d592443deffde82fc8e5528822b454f3143d9b
Erick Torres <erick_torres@brown.edu>
2024-05-21 19:04:26 -0500

    refactor: begin move to NextJS

---
+++ printerform/src/app/upload/page.tsx
@@ -0,0 +1,11 @@
+import { StlViewer } from "react-stl-viewer";
+
+const Upload = () => {
+    return (
+        <div className="min-h-screen w-full bg-emerald-700">
+            Something
+        </div>
+    );
+};
+
+export default Upload;
