1d267bd50918382c803f303c82827c090bc8438a
ErickTorresBrownU <erick_torres@brown.edu>
2024-05-27 17:34:35 -0500

    refactor/fix: create local copy of react-stl-viewer and resolve rotation issues

---
+++ printerform/src/app/stl-viewer-src/StlViewer/StlViewer.tsx
@@ -0,0 +1,70 @@
+import React, { HTMLProps } from 'react'
+import { Canvas } from '@react-three/fiber'
+import SceneSetup, { SceneSetupProps } from './SceneSetup'
+import ErrorBoundary from './ErrorBoundary'
+import { PCFSoftShadowMap } from 'three'
+
+type DivProps = Omit<HTMLProps<HTMLDivElement>, 'color' | 'onError'>
+
+export interface StlViewerProps extends DivProps, SceneSetupProps {
+    onError?: (err: Error) => void
+    canvasId?: string
+}
+
+const StlViewer: React.FC<StlViewerProps> = (
+    {
+        url,
+        cameraProps,
+        modelProps,
+        floorProps,
+        children,
+        onError,
+        extraHeaders,
+        onFinishLoading,
+        canvasId,
+        shadows,
+        objectRespectsFloor,
+        showAxes,
+        orbitControls,
+        cameraInitialPosition,
+        ...otherProps
+    }
+) => {
+    const sceneProps: SceneSetupProps = {
+        url,
+        cameraProps,
+        modelProps,
+        floorProps,
+        extraHeaders,
+        onFinishLoading,
+        shadows,
+        objectRespectsFloor,
+        showAxes,
+        orbitControls,
+        cameraInitialPosition
+    }
+
+    return (
+        <div {...otherProps}>
+            <ErrorBoundary onError={onError}>
+                <React.Suspense fallback={null}>
+                    <Canvas
+                        shadows
+                        gl={{
+                            preserveDrawingBuffer: true,
+                            shadowMapType: PCFSoftShadowMap,
+                            antialias: true
+                        }}
+                        id={canvasId}
+                        style={{ width: '100%', height: '100%' }}
+                    >
+                        <SceneSetup {...sceneProps} />
+                        {children}
+                    </Canvas>
+                </React.Suspense>
+            </ErrorBoundary>
+        </div>
+    )
+}
+
+export default StlViewer
