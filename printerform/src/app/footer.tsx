2e320de72a13020f4b2250f8cb628e5f6d0dec1f
Jaclyn Cohen :) <105396093+jackieac04@users.noreply.github.com>
2024-09-30 10:27:23 -0400

    lofi edits

--- printerform/src/app/footer.tsx
+++ printerform/src/app/footer.tsx
@@ -3,18 +3,21 @@
 import { usePathname } from "next/navigation";
 
 const Footer = () => {
-    const pathname = usePathname();
+  const pathname = usePathname();
 
-    const EXCLUDED_PATHNAMES = ['/upload', '/export'];
-    if (EXCLUDED_PATHNAMES.some(excludedPathname => pathname === excludedPathname)) {
-        return null;
-    }
+  const EXCLUDED_PATHNAMES = ["/upload", "/export"];
+  if (
+    EXCLUDED_PATHNAMES.some((excludedPathname) => pathname === excludedPathname)
+  ) {
+    return null;
+  }
 
-    return (
-        <div className="flex items-center justify-center bg-purple-500 w-full min-h-48">
-            <h1>FOOTER AREA</h1>
-        </div>
-    );
+  return (
+    <div className="flex items-center justify-between px-[24px] bg-[#eeeeee] w-full min-h-48 text-black">
+      <div>logo</div>
+      <div>right side</div>
+    </div>
+  );
 };
 
 export default Footer;
--- printerform/src/app/navbar.tsx
+++ printerform/src/app/navbar.tsx
@@ -3,39 +3,40 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 
 type NavbarItemProps = {
-    text: string,
-    href: string,
+  text: string;
+  href: string;
 };
 
 const NavbarItem = ({ text, href }: NavbarItemProps) => {
-    return (
-        <Link href={href} className="bg-blue-500 hover:text-gray-300 transition duration-200">
-            <p className="text-lg">{text}</p>
-        </Link>
-    );
+  return (
+    <Link href={href} className="hover:text-gray-300 transition duration-200">
+      <p className="text-lg">{text}</p>
+    </Link>
+  );
 };
 
 export const Navbar = () => {
-    const pathname = usePathname();
+  const pathname = usePathname();
 
-    const EXCLUDED_PATHNAMES = ['/upload', '/export'];
-    if (EXCLUDED_PATHNAMES.some(excludedPathname => pathname === excludedPathname)) {
-        return null;
-    }
+  const EXCLUDED_PATHNAMES = ["/upload", "/export"];
+  if (
+    EXCLUDED_PATHNAMES.some((excludedPathname) => pathname === excludedPathname)
+  ) {
+    return null;
+  }
 
-    return (
-        <div className="w-full fixed flex items-center justify-between p-5 bg-red-500 z-50">
-            <Link href="/">
-                <p>Printer Form (Logo here eventually)</p>
-            </Link>
-            <div className="flex items-center space-x-3 bg-green-500">
-                {pathname !== "/" && (
-                    <NavbarItem text="Home" href="/" />
-                )}
-                <NavbarItem text="How To" href="how-to" />
-                <NavbarItem text="Resources" href="resources" />
-                <NavbarItem text="About" href="about" />
-            </div>
-        </div>
-    );
+  return (
+    <div className="w-full fixed flex items-center justify-between px-12 py-8 z-50 text-black">
+      <Link href="/">
+        <p>Printer Form (Logo here eventually)</p>
+      </Link>
+      <div className="flex items-center space-x-10">
+        {pathname !== "/" && <NavbarItem text="Home" href="/" />}
+        <NavbarItem text="How To" href="how-to" />
+        <NavbarItem text="Resources" href="resources" />
+        <NavbarItem text="About" href="about" />
+        <NavbarItem text="Upload" href="upload" />
+      </div>
+    </div>
+  );
 };
