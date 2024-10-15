2ef7c5580fad1176dcb61f078249ee50bd66d294
havi-nguyen <113549784+havi-nguyen@users.noreply.github.com>
2024-10-15 08:34:24 -0400

    add resources, populate about page, change home

--- printerform/src/app/about/page.tsx
+++ printerform/src/app/about/page.tsx
@@ -1,4 +1,9 @@
 import React from "react";
+import xinyu from "../assets/Xinyu.jpeg";
+import havi from "../assets/havi.jpeg";
+import lynda from "../assets/lynda.jpeg";
+import jackie from "../assets/jackie.jpeg";
+import sophia from "../assets/sophia.jpeg";
 
 const About = () => {
   return (
@@ -6,10 +11,8 @@ const About = () => {
       <div className="flex flex-col items-center">
         <h1 className="text-8xl mt-20 mb-32 text-center text-black">About</h1>
       </div>
-
       <div className="max-w-7xl w-full mx-auto">
         <h2 className="text-6xl text-black text-left mb-12">Our Team</h2>
-
         <div className="flex justify-start space-x-10 mb-12">
           <div>
             <img
@@ -17,9 +20,9 @@ const About = () => {
               alt="Ian Gonsher"
               className="object-cover max-w-[300px] min-w-[300px] min-h-[350px] max-h-[350px] w-full h-auto"
             />
-            <h3 className="text-left text-black font-bold mt-8 text-xl">
+            <p className="text-left text-4xl text-black font-bold mt-8 text-xl">
               Ian Gonsher
-            </h3>
+            </p>
             <p className="text-left text-lg text-black mt-4">
               Ian Gonsher is Assistant Professor of Practice in the School of
               Engineering and Department of Computer Science at Brown
@@ -38,22 +41,72 @@ const About = () => {
               .
             </p>
           </div>
-
           <div>
             <img
-              src="https://printerform.com/pictures/profiles/AlaraKalfazade.jpg"
-              alt="Alara Kalfazade"
+              src={xinyu.src}
+              alt="Xinyu Zhou"
               className="object-cover max-w-[300px] min-w-[300px] min-h-[350px] max-h-[350px] w-full h-auto"
             />
             <p className="text-left text-4xl text-black font-bold mt-8 text-xl">
-              Alara Kalfazade
+              Xinyu Zhou
             </p>
             <p className="text-left text-lg text-black mt-4">
-              Alara Kalfazade is a senior at Brown University studying Computer
-              Science-Economics. She is from Istanbul, Turkey. She is passionate
-              about UI/UX and graphic design. View Alara's website{" "}
+              Xinyu Zhou is a master’s student at Brown University studying
+              Computer Science and a graduate of the University of
+              Wisconsin-Madison. She is passionate about merging creativity with
+              technology through UI/UX design. And she is also interested in the
+              transformative potential of AI in healthcare.
+            </p>
+          </div>
+          <div className="max-w-[300px]">
+            <img
+              src={lynda.src}
+              alt="Lynda Umuhoza"
+              className="object-cover max-w-[300px] min-w-[300px] min-h-[350px] max-h-[350px] w-full h-auto"
+            />
+            <p className="text-left text-4xl text-black font-bold mt-8 text-xl">
+              Lynda Winnie Umuhoza
+            </p>
+            <p className="text-left text-lg text-black mt-4">
+              Lynda is a senior at Brown University studying Computer Science.
+              She is from Kigali, Rwanda. Lynda is particularly interested in
+              Artificial Intelligence, software principles, and the ethical use
+              of technology.
+            </p>
+          </div>
+        </div>
+        <div className="flex justify-start space-x-10 mb-12">
+          <div>
+            <img
+              src={havi.src}
+              alt="Havi Nguyen"
+              className="object-cover max-w-[300px] min-w-[300px] min-h-[350px] max-h-[350px] w-full h-auto"
+            />
+            <p className="text-left text-4xl text-black font-bold mt-8 text-xl">
+              Havi Nguyen
+            </p>
+            <p className="text-left text-lg text-black mt-4">
+              Havi Nguyen is a junior at Brown University studying Computer
+              Science, with a focus on the connection between user interaction
+              and tech. She is fascinated by UI/UX design and enjoys creating
+              intuitive digital experiences.
+            </p>
+          </div>
+          <div>
+            <img
+              src={jackie.src}
+              alt="Jaclyn Cohen"
+              className="object-cover max-w-[300px] min-w-[300px] min-h-[350px] max-h-[350px] w-full h-auto"
+            />
+            <p className="text-left text-4xl text-black font-bold mt-8 text-xl">
+              Jaclyn Cohen
+            </p>
+            <p className="text-left text-lg text-black mt-4">
+              Jaclyn Cohen is a junior at Brown University studying Computer
+              Science and Visual Arts. She is passionate about intersections
+              between technology and the arts. You can view her work{" "}
               <a
-                href="https://akalfaza.github.io/"
+                href="https://jaclyncohen.dev/"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="text-blue-600 hover:text-blue-800 underline"
@@ -63,28 +116,25 @@ const About = () => {
               .
             </p>
           </div>
-
           <div className="max-w-[300px]">
             <img
-              src="https://printerform.com/pictures/profiles/CatherinaNiu.JPEG"
-              alt="Catherina Niu"
+              src={sophia.src}
+              alt="Sophia Lim"
               className="object-cover max-w-[300px] min-w-[300px] min-h-[350px] max-h-[350px] w-full h-auto"
             />
             <p className="text-left text-4xl text-black font-bold mt-8 text-xl">
-              Catherina Niu
+              Sophia Lim
             </p>
             <p className="text-left text-lg text-black mt-4">
-              Ian Gonsher is Assistant Professor of Practice in the School of
-              Engineering and Department of Computer Science at Brown
-              University. His teaching and research interests examine creative
-              process as applied to interdisciplinary design practices, and the
-              development of emerging and speculative technologies.
+              Sophia Lim is a junior at Brown University studying Computer
+              Science - Economics. She is originally from Auckland, New Zealand
+              and is particularly interested in AI, Design, and Human-Computer
+              Interaction.
             </p>
           </div>
         </div>
-
         <div className="flex justify-start space-x-10 mb-12">
-          <div>
+          <div className="max-w-[300px]">
             <img
               src="https://printerform.com/pictures/profiles/JoseSandoval.jpg"
               alt="Jose Sandoval"
@@ -109,7 +159,52 @@ const About = () => {
               .
             </p>
           </div>
-
+        </div>
+        <h2 className="text-6xl text-black text-left mb-12">
+          Past Contributors
+        </h2>
+        <div className="flex justify-start space-x-10 mb-12">
+          <div className="max-w-[300px]">
+            <img
+              src="https://printerform.com/pictures/profiles/AlaraKalfazade.jpg"
+              alt="Alara Kalfazade"
+              className="object-cover max-w-[300px] min-w-[300px] min-h-[350px] max-h-[350px] w-full h-auto"
+            />
+            <p className="text-left text-4xl text-black font-bold mt-8 text-xl">
+              Alara Kalfazade
+            </p>
+            <p className="text-left text-lg text-black mt-4">
+              Alara Kalfazade is a senior at Brown University studying Computer
+              Science-Economics. She is from Istanbul, Turkey. She is passionate
+              about UI/UX and graphic design. View Alara's website{" "}
+              <a
+                href="https://akalfaza.github.io/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-blue-600 hover:text-blue-800 underline"
+              >
+                here
+              </a>
+              .
+            </p>
+          </div>
+          <div className="max-w-[300px]">
+            <img
+              src="https://printerform.com/pictures/profiles/CatherinaNiu.JPEG"
+              alt="Catherina Niu"
+              className="object-cover max-w-[300px] min-w-[300px] min-h-[350px] max-h-[350px] w-full h-auto"
+            />
+            <p className="text-left text-4xl text-black font-bold mt-8 text-xl">
+              Catherina Niu
+            </p>
+            <p className="text-left text-lg text-black mt-4">
+              Ian Gonsher is Assistant Professor of Practice in the School of
+              Engineering and Department of Computer Science at Brown
+              University. His teaching and research interests examine creative
+              process as applied to interdisciplinary design practices, and the
+              development of emerging and speculative technologies.
+            </p>
+          </div>
           <div>
             <img
               src="https://printerform.com/pictures/profiles/KatieLynch.jpg"
@@ -127,7 +222,7 @@ const About = () => {
               for collaboration between the digital and physical realms of
               design. View Katie's website{" "}
               <a
-                href="katielynchdesign.com"
+                href="http://katielynchdesign.com"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="text-blue-600 hover:text-blue-800 underline"
@@ -137,130 +232,17 @@ const About = () => {
               .
             </p>
           </div>
-
-          <div className="max-w-[300px]">
-            <img
-              src="https://printerform.com/pictures/profiles/VivianLi.jpg"
-              alt="Vivian Li"
-              className="object-cover max-w-[300px] min-w-[300px] min-h-[350px] max-h-[350px] w-full h-auto"
-            />
-            <p className="text-left text-4xl text-black font-bold mt-8 text-xl">
-              Vivian Li
-            </p>
-            <p className="text-left text-lg text-black mt-4">Vivian Li</p>
-          </div>
         </div>
-
-        <div className="flex justify-start space-x-10 mb-12">
-          <div>
-            <img
-              src="https://via.placeholder.com/200x350"
-              alt="Image 1"
-              className="object-cover max-w-[300px] min-w-[300px] min-h-[350px] max-h-[350px] w-full h-auto"
-            />
-            <p className="text-left text-4xl text-black font-bold mt-8 text-xl">
-              Ian Gonsher
-            </p>
-            <p className="text-left text-lg text-black mt-4">
-              Ian Gonsher is Assistant Professor of Practice in the School of
-              Engineering and Department of Computer Science at Brown
-              University. His teaching and research interests examine creative
-              process as applied to interdisciplinary design practices, and the
-              development of emerging and speculative technologies.
-            </p>
-          </div>
-
-          <div>
-            <img
-              src="https://via.placeholder.com/300x300"
-              alt="Image 2"
-              className="object-cover max-w-[300px] min-w-[300px] min-h-[350px] max-h-[350px] w-full h-auto"
-            />
-            <p className="text-left text-4xl text-black font-bold mt-8 text-xl">
-              Ian Gonsher
-            </p>
-            <p className="text-left text-lg text-black mt-4">
-              Ian Gonsher is Assistant Professor of Practice in the School of
-              Engineering and Department of Computer Science at Brown
-              University. His teaching and research interests examine creative
-              process as applied to interdisciplinary design practices, and the
-              development of emerging and speculative technologies.
-            </p>
-          </div>
-
-          <div className="max-w-[300px]">
-            <img
-              src="https://via.placeholder.com/300x350"
-              alt="Image 3"
-              className="object-cover max-w-[300px] min-w-[300px] min-h-[350px] max-h-[350px] w-full h-auto"
-            />
-            <p className="text-left text-4xl text-black font-bold mt-8 text-xl">
-              Ian Gonsher
-            </p>
-            <p className="text-left text-lg text-black mt-4">
-              Ian Gonsher is Assistant Professor of Practice in the School of
-              Engineering and Department of Computer Science at Brown
-              University. His teaching and research interests examine creative
-              process as applied to interdisciplinary design practices, and the
-              development of emerging and speculative technologies.
-            </p>
-          </div>
-        </div>
-
-        <div className="flex justify-start space-x-10 mb-12">
-          <div>
-            <img
-              src="https://via.placeholder.com/200x350"
-              alt="Image 1"
-              className="object-cover max-w-[300px] min-w-[300px] min-h-[350px] max-h-[350px] w-full h-auto"
-            />
-            <p className="text-left text-4xl text-black font-bold mt-8 text-xl">
-              Ian Gonsher
-            </p>
-            <p className="text-left text-lg text-black mt-4">
-              Ian Gonsher is Assistant Professor of Practice in the School of
-              Engineering and Department of Computer Science at Brown
-              University. His teaching and research interests examine creative
-              process as applied to interdisciplinary design practices, and the
-              development of emerging and speculative technologies.
-            </p>
-          </div>
-
-          <div>
-            <img
-              src="https://via.placeholder.com/300x300"
-              alt="Image 2"
-              className="object-cover max-w-[300px] min-w-[300px] min-h-[350px] max-h-[350px] w-full h-auto"
-            />
-            <p className="text-left text-4xl text-black font-bold mt-8 text-xl">
-              Ian Gonsher
-            </p>
-            <p className="text-left text-lg text-black mt-4">
-              Ian Gonsher is Assistant Professor of Practice in the School of
-              Engineering and Department of Computer Science at Brown
-              University. His teaching and research interests examine creative
-              process as applied to interdisciplinary design practices, and the
-              development of emerging and speculative technologies.
-            </p>
-          </div>
-
-          <div className="max-w-[300px]">
-            <img
-              src="https://via.placeholder.com/300x350"
-              alt="Image 3"
-              className="object-cover max-w-[300px] min-w-[300px] min-h-[350px] max-h-[350px] w-full h-auto"
-            />
-            <p className="text-left text-4xl text-black font-bold mt-8 text-xl">
-              Ian Gonsher
-            </p>
-            <p className="text-left text-lg text-black mt-4">
-              Ian Gonsher is Assistant Professor of Practice in the School of
-              Engineering and Department of Computer Science at Brown
-              University. His teaching and research interests examine creative
-              process as applied to interdisciplinary design practices, and the
-              development of emerging and speculative technologies.
-            </p>
-          </div>
+        <div className="max-w-[300px]">
+          <img
+            src="https://printerform.com/pictures/profiles/VivianLi.jpg"
+            alt="Vivian Li"
+            className="object-cover max-w-[300px] min-w-[300px] min-h-[350px] max-h-[350px] w-full h-auto"
+          />
+          <p className="text-left text-4xl text-black font-bold mt-8 text-xl">
+            Vivian Li
+          </p>
+          <p className="text-left text-lg text-black mt-4"></p>
         </div>
       </div>
     </div>
--- printerform/src/app/page.tsx
+++ printerform/src/app/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 import "../app/globals.css";
+import fscube from "./assets/fscube-bunny.png";
+import curved from "./assets/curved-bunny.png";
 
 const Home = () => {
   return (
@@ -14,10 +16,21 @@ const Home = () => {
         </section>
       </section>
       <section className="flex flex-col items-center justify-center px-[10%] pb-[24px]">
+<<<<<<< HEAD
         <h2 className="text-8xl pb-[30px]">What is PrinterForm?</h2>
         <section className="flex w-full mb-[60px] justify-center">
           {/* <div className="w-[15%] bg-green-400 h-[200px] rounded mr-[5%]"></div> */}
           <p className="text-2xl w-[70%]">
+=======
+        <h2 className="text-8xl pb-[30px]">Mission</h2>
+        <section className="flex items-center w-full">
+          <img
+            src={fscube.src}
+            alt="FSCube Bunny"
+            className="w-[30%] h-[350px] object-overflow mr-[5%] rounded"
+          />
+          <p className="text-2xl w-[60%]">
+>>>>>>> 8f228bd (add resources, populate about page, change home)
             We aim to create a more accessible tool that introduces basic 3D
             modeling and 3D printing into educational and creative spaces. It
             does not replace traditional plastic 3D printing but it hopefully
@@ -26,6 +39,7 @@ const Home = () => {
             8.5”x11” paper, and PrinterForm anyone can practice their 3D
             modeling skills and produce high speed, low resolution 3D objects.
           </p>
+<<<<<<< HEAD
           {/* <div className="w-[15%] bg-green-400 h-[200px] rounded ml-[5%]"></div> */}
         </section>
         <section className="justify-center items-center">
@@ -42,6 +56,20 @@ const Home = () => {
           <div className="bg-purple-400 w-[300px] h-[200px] rounded drop-shadow"></div>
         </section> 
       </section> */}
+=======
+          <img
+            src={curved.src}
+            alt="Curved Bunny"
+            className="w-[25%] h-[350px] object-contain ml-[5%] rounded"
+          />
+        </section>
+      </section>
+      <section className="justify-center items-center">
+        <button className="text-8xl bg-purple-600 rounded-xl px-[24px] py-[32px] text-white drop-shadow ">
+          GET STARTED
+        </button>
+      </section>
+>>>>>>> 8f228bd (add resources, populate about page, change home)
     </main>
   );
 };
