9a405267a9ca670616f4f6da7456346ee821baed
Sophia Lim <sophia_lim@brown.edugit config --global user.name Sophia Lim>
2024-10-20 11:40:23 -0400

    added images

--- printerform/src/app/page.tsx
+++ printerform/src/app/page.tsx
@@ -2,6 +2,9 @@
 import "../app/globals.css";
 import fscube from "./assets/fscube-bunny.png";
 import curved from "./assets/curved-bunny.png";
+import foursided from "./assets/IMG_7563.jpg";
+import curvedBunny from "./assets/IMG_7706.jpg";
+import all from "./assets/IMG_7724.jpg";
 
 const Home = () => {
   return (
@@ -15,22 +18,15 @@ const Home = () => {
           ></video>
         </section>
       </section>
-      <section className="flex flex-col items-center justify-center px-[10%] pb-[24px]">
-<<<<<<< HEAD
-        <h2 className="text-8xl pb-[30px]">What is PrinterForm?</h2>
-        <section className="flex w-full mb-[60px] justify-center">
-          {/* <div className="w-[15%] bg-green-400 h-[200px] rounded mr-[5%]"></div> */}
-          <p className="text-2xl w-[70%]">
-=======
+      <section className="flex flex-col items-center justify-center px-[10%] pb-[24px] text-center">
         <h2 className="text-8xl pb-[30px]">Mission</h2>
-        <section className="flex items-center w-full">
-          <img
+        <section className="flex flex-col items-center w-full">
+          {/* <img
             src={fscube.src}
             alt="FSCube Bunny"
             className="w-[30%] h-[350px] object-overflow mr-[5%] rounded"
-          />
+          /> */}
           <p className="text-2xl w-[60%]">
->>>>>>> 8f228bd (add resources, populate about page, change home)
             We aim to create a more accessible tool that introduces basic 3D
             modeling and 3D printing into educational and creative spaces. It
             does not replace traditional plastic 3D printing but it hopefully
@@ -39,29 +35,11 @@ const Home = () => {
             8.5”x11” paper, and PrinterForm anyone can practice their 3D
             modeling skills and produce high speed, low resolution 3D objects.
           </p>
-<<<<<<< HEAD
-          {/* <div className="w-[15%] bg-green-400 h-[200px] rounded ml-[5%]"></div> */}
-        </section>
-        <section className="justify-center items-center">
-          <button className="text-8xl bg-purple-600 rounded-xl px-[24px] py-[32px] text-white drop-shadow ">
-            <a href="how-to">GET STARTED</a>
-          </button>
-        </section>
-      </section>
-      {/* <section className="flex flex-col items-center">
-        <h2 className="text-8xl pb-[30px]">Case Studies</h2>
-        <section className="gap-[60px] flex">
-          <div className="bg-green-400 w-[300px] h-[200px] rounded drop-shadow"></div>
-          <div className="bg-blue-400 w-[300px] h-[200px] rounded drop-shadow"></div>
-          <div className="bg-purple-400 w-[300px] h-[200px] rounded drop-shadow"></div>
-        </section> 
-      </section> */}
-=======
-          <img
+          {/* <img
             src={curved.src}
             alt="Curved Bunny"
             className="w-[25%] h-[350px] object-contain ml-[5%] rounded"
-          />
+          /> */}
         </section>
       </section>
       <section className="justify-center items-center">
@@ -69,7 +47,25 @@ const Home = () => {
           GET STARTED
         </button>
       </section>
->>>>>>> 8f228bd (add resources, populate about page, change home)
+      <section className="flex flex-col items-center">
+        <section className="gap-[60px] flex">
+          <img
+            src={foursided.src}
+            alt="foursided"
+            className="bg-white-400 w-[300px] h-[200px] rounded drop-shadow"
+          ></img>
+          <img
+            src={all.src}
+            alt="all examples"
+            className="bg-white-400 w-[300px] h-[200px] rounded drop-shadow"
+          ></img>
+          <img
+            src={curvedBunny.src}
+            alt="curved Bunny"
+            className="bg-white-400 w-[300px] h-[200px] rounded drop-shadow"
+          ></img>
+        </section>
+      </section>
     </main>
   );
 };
