e4bc07f14a949f4d5fdd019640d84ec489f53c61
havi-nguyen <113549784+havi-nguyen@users.noreply.github.com>
2024-10-22 08:49:43 -0400

    update fonts, colors, margins

--- printerform/src/app/page.tsx
+++ printerform/src/app/page.tsx
@@ -5,6 +5,7 @@ import curved from "./assets/curved-bunny.png";
 import foursided from "./assets/IMG_7563.jpg";
 import curvedBunny from "./assets/IMG_7706.jpg";
 import all from "./assets/IMG_7724.jpg";
+import poster from "./assets/video_display.jpg";
 
 const Home = () => {
   return (
@@ -14,20 +15,22 @@ const Home = () => {
           <video
             controls
             loop
-            className="w-[80%] self-center rounded drop-shadow"
+            className="w-[80%] self-center rounded"
             src="/IntroVideoV2.mp4"
+            poster={poster.src}
+            style={{ objectFit: "cover" }}
           ></video>
         </section>
       </section>
       <section className="flex flex-col items-center justify-center px-[10%] pb-[24px] text-center">
-        <h2 className="text-8xl pb-[30px]">Mission</h2>
+        <h2
+          className="font-fatfrank text-8xl pb-[30px]"
+          style={{ color: "#40001C" }}
+        >
+          Mission
+        </h2>
         <section className="flex flex-col items-center w-full">
-          {/* <img
-            src={fscube.src}
-            alt="FSCube Bunny"
-            className="w-[30%] h-[350px] object-overflow mr-[5%] rounded"
-          /> */}
-          <p className="text-2xl w-[60%]">
+          <p className="text-2xl w-[60%]" style={{ color: "#40001C" }}>
             We aim to create a more accessible tool that introduces basic 3D
             modeling and 3D printing into educational and creative spaces. It
             does not replace traditional plastic 3D printing but it hopefully
@@ -36,18 +39,9 @@ const Home = () => {
             8.5”x11” paper, and PrinterForm anyone can practice their 3D
             modeling skills and produce high speed, low resolution 3D objects.
           </p>
-          {/* <img
-            src={curved.src}
-            alt="Curved Bunny"
-            className="w-[25%] h-[350px] object-contain ml-[5%] rounded"
-          /> */}
         </section>
       </section>
-      <section className="justify-center items-center">
-        <button className="text-8xl bg-red-600 rounded-xl px-[24px] py-[32px] text-white drop-shadow ">
-          <a href="/how-to">GET STARTED</a>
-        </button>
-      </section>
+
       <section className="flex flex-col items-center">
         <section className="gap-[60px] flex">
           <img
@@ -63,9 +57,17 @@ const Home = () => {
           <img
             src={curvedBunny.src}
             alt="curved Bunny"
-            className="bg-white-400 w-[300px] h-[200px] rounded drop-shadow"
+            className="mb-[50px] bg-white-400 w-[300px] h-[200px] rounded drop-shadow"
           ></img>
         </section>
+      </section>
+      <section className="justify-center items-center">
+        <button
+          className="font-fatfrank text-7xl bg-red-600 rounded-xl px-[24px] py-[32px] text-white drop-shadow transform transition-all duration-300 ease-in-out hover:bg-red-500 hover:scale-105"
+          style={{ backgroundColor: "#D80029" }}
+        >
+          <a href="/how-to">GET STARTED</a>
+        </button>
       </section>
     </main>
   );
