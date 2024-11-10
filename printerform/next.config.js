c5bda0d0ded78f410a610c07a036acb599a5d26b
xinyu zhou <xinyuzhou@macbookpro.mynetworksettings.com>
2024-11-10 18:13:34 -0500

    next.config updated

---
+++ printerform/next.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    basePath: '/printerForm1.0',   // Adjust based on your repository name
+    assetPrefix: '/printerForm1.0',  // Adjust based on your repository name
+    output: 'export',
+  };
+  
