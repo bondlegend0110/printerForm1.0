0b17feac24fc9a51797ca93c6fa29739a26319b3
xinyu zhou <xinyuzhou@macbookpro.mynetworksettings.com>
2024-11-10 19:17:05 -0500

    fix: removed base branch

--- printerform/next.config.js
+++ printerform/next.config.js
@@ -1,6 +1,4 @@
 module.exports = {
-    basePath: '/printerForm1.0',   // Adjust based on your repository name
-    assetPrefix: '/printerForm1.0',  // Adjust based on your repository name
     output: 'export',
   };
   
