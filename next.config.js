13cbae18a93f8c3935d84444ac9b9b352098c867
Umuhoza Winnie Lynda <lynda_umuhoza@brown.edu>
2024-11-18 17:13:49 -0500

    fixing pictures

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   basePath: "/printerForm1.0",
-  output: "export", // <=== enables static exports
+  output: " ", // <=== enables static exports
   reactStrictMode: true,
   assetPrefix: "https://printerform1.github.io/printerForm1.0",
 };
