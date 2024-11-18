d7b6fe985623e1cfcf58a1fe7e8ad8840fd25228
havi-nguyen <113549784+havi-nguyen@users.noreply.github.com>
2024-11-18 11:45:16 -0500

    changed next.js

--- next.config.js
+++ next.config.js
@@ -5,8 +5,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   basePath: "/printerForm1.0",
-  output: "export",  // <=== enables static exports
+  output: "export", // <=== enables static exports
   reactStrictMode: true,
+  assetPrefix: "/printerForm1.0/",
 };
 
 module.exports = nextConfig;
