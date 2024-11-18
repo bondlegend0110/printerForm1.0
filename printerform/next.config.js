a12ff3e3673b38013603e0140f63f0abba4a4d61
xinyu zhou <xinyuzhou@xinyus-MacBook-Pro-8.local>
2024-11-18 11:36:48 -0500

    revert

--- printerform/next.config.js
+++ printerform/next.config.js
@@ -2,8 +2,5 @@ module.exports = {
     output: 'export',
     assetPrefix: '/printerForm1.0/' ,
     basePath: '/printerForm1.0' ,
-    images: {
-      unoptimized: true, // Needed for Next.js image optimization during export
-    },
   };
   
