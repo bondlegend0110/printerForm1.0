0f23faf0f00dc316c2c2b6f0fd03349f5edbcba2
xinyu zhou <xinyuzhou@macbookpro.mynetworksettings.com>
2024-11-17 20:27:59 -0500

    next.config added and _next/static/ANszLhzaSPrrBl8kcvdTV/

---
+++ next.config.js
@@ -0,0 +1,12 @@
+// module.exports = {
+//     output: 'export',
+//     basePath: '',
+//   };
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  basePath: "/printerForm1.0",
+  output: "export",  // <=== enables static exports
+  reactStrictMode: true,
+};
+
+module.exports = nextConfig;
