576feafb5f7110069061b6c54560af09dbe9ddd1
Umuhoza Winnie Lynda <lynda_umuhoza@brown.edu>
2024-11-25 21:15:14 -0500

    fixing alignment

--- printerform/src/app/export/page.tsx
+++ printerform/src/app/export/page.tsx
@@ -806,9 +806,9 @@ class PrintableFactory {
 
 
            // top model
-           renderSide(0, true, false, 80, 50, 0.9);
+           renderSide(0, true, false, 80, 45, 0.9);
            // bottom model
-           renderSide(Math.PI, false, true, 80, 50, 0.9);
+           renderSide(Math.PI, false, true, 80, 45, 0.9);
 
 
         //    // top model
@@ -916,9 +916,9 @@ class PrintableFactory {
 
 
            // top model
-           renderSide(Math.PI/2, true, false, 80, 360, 0.9);
+           renderSide(Math.PI/2, true, false, 80, 45, 0.9);
            // bottom model
-           renderSide(-Math.PI/2, false, true, 80, 360, 0.9);
+           renderSide(-Math.PI/2, false, true, 80, 45, 0.9);
 
 
         //    // top model
