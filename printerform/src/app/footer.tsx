b7d50ea7d141b61ae66f93cd306a583f3d20f14d
Umuhoza Winnie Lynda <lynda_umuhoza@brown.edu>
2024-11-26 09:09:37 -0500

    feedback form

--- printerform/src/app/footer.tsx
+++ printerform/src/app/footer.tsx
@@ -16,13 +16,14 @@ const Footer = () => {
     <div className="flex items-center justify-end px-[24px] bg-[#ffffff] w-full min-h-40 text-black ">
       {/* <img src="logo.png" className="w-[100px] shadow-none" alt="" /> */}
       <div>
-        <a href="mailto:printerform1@gmail.com" target="_blank">
+        {/* <a href="mailto:printerform1@gmail.com" target="_blank">
           printerform1@gmail.com
         </a>{" "}
-        |{" "}
+        |{" "} */}
         <a
           href="https://docs.google.com/forms/d/e/1FAIpQLScCbfCI4AGwUjoJ6NzdIvLrkpYKWgth8RgtbY9oTs-fHyMbQw/viewform"
           target="_blank"
+          style={{ color: "#D80029" }}
         >
           Feedback Form
         </a>
